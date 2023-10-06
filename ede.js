2f50bdca6bafffb084b796d0063c73468c1b2c05
Roshan Manuel <31125563+Roesh@users.noreply.github.com>
2023-10-06 23:09:27 +0000

    feat: get JSON data from excel worksheet

--- ede.js
+++ ede.js
@@ -1,13 +1,35 @@
 var XLSX = require("xlsx");
 
-const testData = [
-    {programName: 'CON-IT'}
-]
+const numberOfEntries = 37
+const mainDataTab = "Program Data"
+
+const testData = Array(numberOfEntries).fill({
+    programName: 'CON-IT',
+    size: 1,
+    overallHealth: 'Green',
+    Schedule: 'Green',
+    Cost: 'Green',
+    Resource: 'Green',
+    Risk: 'Green',
+    Comment: 'string',
+    Risks: 'string',
+    Achievements: 'string'
+})
+
+
 
 XLSX.utils.json_to_sheet(testData)
 const workbook = XLSX.utils.book_new();
 const worksheet = XLSX.utils.json_to_sheet(testData);
 
-XLSX.utils.book_append_sheet(workbook, worksheet, "Program Data");
+XLSX.utils.book_append_sheet(workbook, worksheet, mainDataTab);
 
 XLSX.writeFile(workbook, "test.xlsx");
+
+
+const readWorkbook = XLSX.readFile("test.xlsx");
+const readWorksheet = readWorkbook.Sheets[mainDataTab]
+
+// Convert the worksheet to an array of objects
+const jsonData = XLSX.utils.sheet_to_json(readWorksheet);
+console.log(jsonData)
